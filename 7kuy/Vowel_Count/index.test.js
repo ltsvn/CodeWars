23fed445a4db0d67c6477ada54782b3d352933c2
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-08-25 10:12:07 +0200

    Vowel count

---
+++ 7kuy/Vowel_Count/index.test.js
@@ -0,0 +1,7 @@
+const {assert} = require("chai");
+
+describe("Vowels Count Tests",function(){
+    it("should return 5 for 'abracadabra'",function(){
+        assert.strictEqual(getCount("abracadabra"), 5) ;
+    });
+});
